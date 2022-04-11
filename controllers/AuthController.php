82f23d7d1e57e9aa8dea2c14fbed57fc7485a739
moises <elnaufrago2009@gmail.com>
2022-04-11 13:44:37 -0500

    editing

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -13,11 +13,13 @@ class AuthController extends Controller
   }
 
   public function login(){
+
     $db = new DB('05cr8wjw5112.us-east-1.psdb.cloud', 'vpjfb2ewapce', 'pscale_pw_TZEawdCs7uZZc4OcMBJDVTQ7gdyy9ZHlZRqwCVEbc1U', 'invoice');
 
     //$db->insert('usuarios', ['nombres' => 'linares']);
 
-    $q = $db->select('roles',['id', '=', '1']);
+    $q = $db->query('select * from roles where nombre=?',['name'=>'admin']);
+    //$q = $db->select('roles',['id', '=', '1']);
     print_r($q->all());
     /*if ($db->error() && $db->count() > 0) {
       foreach ($q->all() as $obj){
