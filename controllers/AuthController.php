96d30bc90ca613cfb220b275478ee1ae56ee867f
Moises <elnaufrago2009@gmail.com>
2022-04-11 21:14:00 -0500

    ediutingf

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -16,33 +16,17 @@ class AuthController extends Controller
 
     $db = new DB('05cr8wjw5112.us-east-1.psdb.cloud', 'vpjfb2ewapce', 'pscale_pw_TZEawdCs7uZZc4OcMBJDVTQ7gdyy9ZHlZRqwCVEbc1U', 'invoice');
 
-    $global = $db->query('select usuarios.nombres,
-        usuarios.usuario,
-        usuarios.empresa_id,
-        roles.id,
-        roles.nombre,
-        empresas.razon_social,
-        empresas.ruc,
-        empresas.direccion,
-        empresas.ubigeo
-        from usuarios
-             inner join roles on usuarios.role_id = roles.id
-             inner join empresas on usuarios.empresa_id = empresas.id
-        where usuario = ? && password = ? ',[
-            'usuario' => 'elnaufrago2009@gmail.com',
-            'password' => 'moiseslinar3s'
-      ]);
-    print_r($global->all());
+
 
     //$db->insert('usuarios', ['nombres' => 'linares']);
 
-    /*$q = $db->query('select * from usuarios where usuario = ? && password = ?',[
+    $q = $db->query('select * from usuarios where usuario = ? && password = ?',[
       'usuario' => 'elnaufrago2009@gmail.com',
       'password' => 'moiseslinar3s'
-    ]);*/
+    ]);
 
-    //if ($q->count() == 1){
-      /*$global = $db->query('select usuarios.nombres,
+    if ($q->count() == 1){
+      $global = $db->query('select usuarios.nombres,
         usuarios.usuario,
         usuarios.empresa_id,
         roles.id,
@@ -55,12 +39,11 @@ class AuthController extends Controller
              inner join roles on usuarios.role_id = roles.id
              inner join empresas on usuarios.empresa_id = empresas.id
         where usuario = ? && password = ? ',[
-            'usuario' => 'elnaufrago2009@gmail.com',
-            'password' => 'moiseslinar3s'
-      ]);*/
-      //print_r($global->result());
-      //print_r($global->results());
-    //}
+        'usuario' => 'elnaufrago2009@gmail.com',
+        'password' => 'moiseslinar3s'
+      ]);
+      print_r($global->all());
+    }
     //$q = $db->select('roles',['id', '=', '1']);
     //print_r($q->count());
     /*if ($db->error() && $db->count() > 0) {
