cf054d75a3595cf063e9fbd7f79c08aff6ad54a4
moises <elnaufrago2009@gmail.com>
2022-04-15 00:19:53 -0500

    editing

--- config/Controller.php
+++ config/Controller.php
@@ -10,7 +10,7 @@ class Controller
     public function __construct()
     {
         $this->conn = mysqli_init();
-        $this->conn->ssl_set(NULL, NULL, "/etc/ssl/cert.pem", NULL, NULL);
+        $this->conn->ssl_set(NULL, NULL, "`/etc/ssl/cert.pem`", NULL, NULL);
         $this->conn->real_connect('05cr8wjw5112.us-east-1.psdb.cloud', '1na7g1otd85h', 'pscale_pw_QhscZT4QdaJPzdt_E7XzSA6m4SF6ALVmFBTUNhTOYAw', 'invoice');
     }
     public function resjson($data = [], $status = 200, array $headers = [], $options = 0){
